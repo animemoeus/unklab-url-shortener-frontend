4c0b47de59b97dcb91e8a91014ff0a3b93c9f404
Chester Wicipto <s21710323@student.unklab.ac.id>
2022-01-23 12:03:30 +0800

    tesform

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,19 @@ export default function Home() {
 
       <br />
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui totam
-        debitis minus dolor aliquid harum eligendi obcaecati dicta repellendus
-        earum, a eaque et, fugiat labore quibusdam accusantium excepturi ut.
-      </p>
+      <form className="row g-3">
+  <div className="col-auto">
+    <label for="staticEmail2" className="visually-hidden">Email</label>
+    <input type="text" readonly className="form-control-plaintext" id="staticEmail2" value="email@example.com"/>
+  </div>
+  <div className="col-auto">
+    <label for="inputPassword2" className="visually-hidden">Password</label>
+    <input type="password" className="form-control" id="inputPassword2" placeholder="Password"/>
+  </div>
+  <div className="col-auto">
+    <button type="submit" className="btn btn-primary mb-3">Confirm identity</button>
+  </div>
+</form>
     </div>
   );
 }
