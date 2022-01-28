2d06b5cc42782cfe0d8e35c7d8ca25541e67d9d3
Arter Tendean <arter.tendean.07@gmail.com>
2022-01-28 17:13:30 +0800

    Finished a simple UI for testing the API

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,116 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [inputUrl, setInputUrl] = useState("");
+  const [shortenedUrlResult, setShortenedUrlResult] = useState([
+    "https://waifu.animemoe.us",
+  ]);
+
+  // submit button onClick
+  const handleSubmitButton = () => {
+    setIsLoading(!isLoading);
+
+    // API calling using POST method
+    axios
+      .post(
+        "https://uus.animemoe.us/api/shorten/",
+        {
+          target_url: inputUrl,
+        } // body data
+      )
+      .then((res) => {
+        // append the result to array `shortenedUrlResult`
+        setShortenedUrlResult((shortenedUrlResult) => [
+          ...shortenedUrlResult,
+          `http://localhost:3000/${res.data.slug}`,
+        ]);
+      })
+      .catch((err) => {
+        // error handling
+        if (err.response.status === 400) {
+          alert("Masukan URL yang valid!");
+        } else {
+          alert("Ada masalah. Coba lagi!");
+        }
+      })
+      .finally(() => {
+        // clear the input URL form
+        setInputUrl("");
+
+        // disable button loading animation at the end of API call
+        setIsLoading(false);
+      });
+  };
+
+  // input url onChange
+  const handleInputUrl = (e) => {
+    setInputUrl(e.target.value);
+  };
+
   return (
-    <div>
-      <h3 className="text-center">Unklab URL Shortener</h3>
+    <div className="min-vh-100">
+      <h3 className="text-center mt-4">Unklab URL Shortener</h3>
 
-      <br />
+      {/* container medium */}
+      <div className="container-md px-1">
+        <div className="card mt-4">
+          <div className="card-header text-center">
+            <h4>Iustus Veritas Scientia</h4>
+          </div>
+          <div className="card-body">
+            <div className="input-group mb-3">
+              <input
+                type="url"
+                className="form-control border"
+                placeholder="Masukan URL"
+                value={inputUrl}
+                onChange={(e) => handleInputUrl(e)}
+              />
+              {isLoading === true && (
+                <button
+                  className="btn btn-outline-secondary"
+                  type="button"
+                  disabled
+                  onClick={handleSubmitButton}
+                >
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    role="status"
+                    aria-hidden="true"
+                  ></span>{" "}
+                  Submit
+                </button>
+              )}
+              {isLoading === false && (
+                <button
+                  className="btn btn-outline-secondary"
+                  type="button"
+                  onClick={handleSubmitButton}
+                >
+                  Submit
+                </button>
+              )}
+            </div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui totam
-        debitis minus dolor aliquid harum eligendi obcaecati dicta repellendus
-        earum, a eaque et, fugiat labore quibusdam accusantium excepturi ut.
-      </p>
+            <hr className="m-0" />
+
+            {/* show the shortened URL result */}
+            <ul className="list-group list-group-flush">
+              {shortenedUrlResult.map((url, i) => {
+                return (
+                  <li className="list-group-item" key={i}>
+                    <a href={url}>{url}</a>
+                  </li>
+                );
+              })}
+            </ul>
+            {/* end — show the shortened URL result */}
+          </div>
+        </div>
+      </div>
+      {/* end — container medium */}
     </div>
   );
 }
