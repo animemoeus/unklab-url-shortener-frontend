5d8d7d99caa62b5a5e86dc10e056b4ae7103e50e
Arter Tendean <arter.tendean.07@gmail.com>
2022-02-06 00:34:46 +0800

    connect to API

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,62 @@
+import axios from "axios";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-
-import Navbar from "../components/molecules/Navbar";
 import { useState } from "react";
 
+import Navbar from "../components/molecules/Navbar";
+
 export default function Home() {
+  // default state
+  const [inputUrl, setInputUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [shortenedLink, setShortenedLink] = useState([
-    { original: "https://animemoe.us", result: "https://animemoe.us" },
-    { original: "https://animemoe.us", result: "https://animemoe.us" },
+    // { original: "https://animemoe.us", result: "https://animemoe.us" },
+    // { original: "https://animemoe.us", result: "https://animemoe.us" },
   ]);
+
+  // submit button onClick
+  const handleSubmitButton = () => {
+    setIsLoading(!isLoading);
+
+    // API calling using POST method
+    axios
+      .post(
+        "https://uus.animemoe.us/api/shorten/",
+        {
+          target_url: inputUrl,
+        } // body data
+      )
+      .then((res) => {
+        // append the result to array `shortenedLink`
+        setShortenedLink((shortenedUrlResult) => [
+          ...shortenedLink,
+          {
+            original: inputUrl,
+            result: `/${res.data.slug}`,
+          },
+        ]);
+      })
+      .catch((err) => {
+        // error handling
+        console.log(err);
+        if (err.response.status === 400) {
+          alert("Masukan URL yang valid!");
+        } else {
+          alert("Ada masalah. Coba lagi!");
+        }
+      })
+      .finally(() => {
+        // clear the input from URL form
+        setInputUrl("");
+
+        // disable button loading animation at the end of API call
+        setIsLoading(false);
+      });
+  };
+
+  // input url onChange
+  const handleInputUrl = (e) => {
+    setInputUrl(e.target.value);
+  };
 
   return (
     <div>
@@ -28,13 +77,41 @@ export default function Home() {
         <div className="border rounded container-md shadow-sm my-2 py-2">
           <div className="row">
             <div className="col-sm-10 my-1">
-              <input type="text" className="form-control" />
+              {/* input text from user */}
+              <input
+                type="text"
+                className="form-control"
+                value={inputUrl}
+                onChange={(e) => handleInputUrl(e)}
+              />
+              {/* End — input text from user */}
             </div>
             <div className="col-sm-2 my-1">
               <div className="d-grid gap-2">
-                <button className="btn btn-blue text-white" type="button">
-                  Shorten
-                </button>
+                {isLoading === true && (
+                  <button
+                    className="btn btn-blue text-white"
+                    type="button"
+                    onClick={handleSubmitButton}
+                    disabled
+                  >
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>{" "}
+                    Loading...
+                  </button>
+                )}
+                {isLoading === false && (
+                  <button
+                    className="btn btn-blue text-white"
+                    type="button"
+                    onClick={handleSubmitButton}
+                  >
+                    Shorten
+                  </button>
+                )}
               </div>
             </div>
             {/* Result */}
@@ -55,8 +132,8 @@ export default function Home() {
                           {e.original}
                         </a>
                       </div>
-                      {/* end — original url */}
-                      {/* shortened url */}
+                      {/* End — original url */}
+                      {/* result url */}
                       <div className="col-sm-5 my-1">
                         <a
                           href={e.result}
@@ -65,7 +142,7 @@ export default function Home() {
                           {e.result}
                         </a>
                       </div>
-                      {/* end — shortened url */}
+                      {/* End — result url */}
                       {/* copy button */}
                       <div className="col-sm-1 my-1">
                         <div className="d-grid">
@@ -79,7 +156,7 @@ export default function Home() {
                           </CopyToClipboard>
                         </div>
                       </div>
-                      {/* end — copy button */}
+                      {/* End — copy button */}
                     </div>
                   </div>
                 );
