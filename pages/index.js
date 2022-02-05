f565faa314849d48962aa40a3cef4ee22f707cfb
Arter Tendean <arter.tendean.07@gmail.com>
2022-02-06 00:09:42 +0800

    adding shortened link result

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,95 @@
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import Navbar from "../components/molecules/Navbar";
+import { useState } from "react";
 
 export default function Home() {
+  const [shortenedLink, setShortenedLink] = useState([
+    { original: "https://animemoe.us", result: "https://animemoe.us" },
+    { original: "https://animemoe.us", result: "https://animemoe.us" },
+  ]);
+
   return (
     <div>
       <Navbar />
 
-      <div className="border">
-        <section className="text-center">
+      {/* Hero Image */}
+      <div className="shadow-sm">
+        <section className="text-center pb-3">
           <img src="/img/logo-unklab.png" alt="..." className="w-50" />
-          <div className="">
-            <h1>Unklab Shortener Link</h1>
-          </div>
+
+          <h1>Unklab Shortener Link</h1>
         </section>
       </div>
+      {/* End — Hero Image */}
 
-      <hr />
-      <hr />
-      <div className="border mt-3">
-        <div className="container mt-3 shadow ">
-          <div className="row g-0">
-            <div className="col-sm-6 col-md-8">
-              <label htmlFor="inputPassword2" className="visually-hidden">
-                Masukan link
-              </label>
-              <input
-                type="text"
-                className="form-control forminput"
-                id="inputPassword2"
-                placeholder="Enter Your Link"
-              />
+      {/* Short Link */}
+      <div className="p-2 mt-1">
+        <div className="border rounded container-md shadow-sm my-2 py-2">
+          <div className="row">
+            <div className="col-sm-10 my-1">
+              <input type="text" className="form-control" />
             </div>
-            <div className="col-6 col-md-4">
-              <div>
-                <input
-                  className="btn buttonshort"
-                  type="submit"
-                  value="Shorted Link"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="row g-0 mt-4 mb-4">
-            <div className="col-sm-6 col-md-8">
-              <div>
-                <label htmlFor="staticEmail2" className="visually-hidden">
-                  Email
-                </label>
-                <input
-                  type="text"
-                  readOnly
-                  className="form-control-plaintext copy"
-                  id="staticEmail2"
-                  value="email@example.com"
-                />
-              </div>
-            </div>
-            <div className="col-6 col-md-4">
-              <CopyToClipboard text="email@example.com">
-                <button className="btn buttonshort" type="submit">
-                  Copy to clipboard
+            <div className="col-sm-2 my-1">
+              <div className="d-grid gap-2">
+                <button className="btn btn-blue text-white" type="button">
+                  Shorten
                 </button>
-              </CopyToClipboard>
+              </div>
             </div>
+            {/* Result */}
+            <div className="rounded mt-2">
+              {shortenedLink.map((e, i) => {
+                return (
+                  <div
+                    className="list-group-item  animate__animated animate__fadeIn"
+                    key={i}
+                  >
+                    <div className="row">
+                      {/* original url */}
+                      <div className="col-sm-6 my-1">
+                        <a
+                          href={e.original}
+                          className="text-decoration-none text-muted"
+                        >
+                          {e.original}
+                        </a>
+                      </div>
+                      {/* end — original url */}
+                      {/* shortened url */}
+                      <div className="col-sm-5 my-1">
+                        <a
+                          href={e.result}
+                          className="text-decoration-none text-muted"
+                        >
+                          {e.result}
+                        </a>
+                      </div>
+                      {/* end — shortened url */}
+                      {/* copy button */}
+                      <div className="col-sm-1 my-1">
+                        <div className="d-grid">
+                          <CopyToClipboard text={e.result}>
+                            <button
+                              type="button"
+                              className="btn btn-outline-success btn-sm shadow-none"
+                            >
+                              Copy
+                            </button>
+                          </CopyToClipboard>
+                        </div>
+                      </div>
+                      {/* end — copy button */}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            {/* End — Result */}
           </div>
         </div>
       </div>
+      {/* End — Short Link */}
       <div>
         <div className="container-fluid text-center">
           <div className="row">
