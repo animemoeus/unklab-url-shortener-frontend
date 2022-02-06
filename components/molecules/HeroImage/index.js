f26d18b1053830696b3efc85d36906fdd21a4819
Arter Tendean <arter.tendean.07@gmail.com>
2022-02-06 12:59:19 +0800

    update HeroImage component

--- components/molecules/HeroImage/index.js
+++ components/molecules/HeroImage/index.js
@@ -1,11 +1,20 @@
+import Image from "next/image";
+
 export default function HeroImage() {
   return (
     <div className="shadow-sm">
-      <section className="text-center pb-3">
-        <img src="/img/logo-unklab.png" alt="..." className="hero-image" />
+      <div className="text-center pb-3">
+        <Image
+          src="/img/logo-unklab.png"
+          alt="Logo Unklab"
+          width={405}
+          height={300}
+          placeholder="blur"
+          blurDataURL="/img/logo-unklab.png"
+        />
 
         <h1>Unklab Shortener Link</h1>
-      </section>
+      </div>
     </div>
   );
 }
