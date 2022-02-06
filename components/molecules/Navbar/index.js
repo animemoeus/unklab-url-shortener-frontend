17886b8a5a768e1bbeaff21c4b50899a78d934aa
Arter Tendean <arter.tendean.07@gmail.com>
2022-02-06 13:01:37 +0800

    update Navbar component

--- components/molecules/Navbar/index.js
+++ components/molecules/Navbar/index.js
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-orange">
+    <nav className="navbar navbar-expand-lg navbar-light bg-orange shadow-sm">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Unklab URL Shortener
