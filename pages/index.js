8716466c4820b11e506818a9a4a3de18f6520436
Arter Tendean <arter.tendean.07@gmail.com>
2022-02-06 11:28:46 +0800

    update hero image

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Home() {
       {/* Hero Image */}
       <div className="shadow-sm">
         <section className="text-center pb-3">
-          <img src="/img/logo-unklab.png" alt="..." className="w-50" />
+          <img src="/img/logo-unklab.png" alt="..." className="hero-image" />
 
           <h1>Unklab Shortener Link</h1>
         </section>
