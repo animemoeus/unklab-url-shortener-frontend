734dc9565df67d1dd98e89106165453d4b935eaa
Arter Tendean <arter.tendean.07@gmail.com>
2022-04-18 19:23:54 +0800

    adding nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import "animate.css";
+import "../styles/nprogress.css";
+
+import Router from "next/router";
+import NProgress from "nprogress";
+
+// nprogress loading
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
