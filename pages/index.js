87b47ca4dec755b2a6897478101968034b0b6ac7
Arter Tendean <arter.tendean.07@gmail.com>
2022-04-20 21:06:49 +0800

    New home page (#56)
    * update login to v2
    
    * update register to v2
    
    * fix errors
    
    * update home page
    
    * update text & color
    
    * adding animation
    
    * update next.config.js
    
    * update home page
    
    * update the code

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,66 @@
-import cookie from "cookie";
-
+// import cookie from "cookie";
 import Navbar from "../components/molecules/Navbar";
-// import HeroImage from "../components/molecules/HeroImage";
+
+
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 // import Shortener from "../components/organisms/Shortener";
+// import HeroImage from "../components/molecules/HeroImage";
 
 export default function Home(props) {
+  const [inputUrl, setInputUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [shortenedLink, setShortenedLink] = useState([]);
+
+  const handleSubmitButton = (button) => {
+    setIsLoading(true);
+
+    // inputUrl cannot be empty
+    if (inputUrl.length <= 0) {
+      alert("Masukan link!");
+      setIsLoading(false);
+      return;
+    }
+
+    // API calling
+    const formdata = new FormData();
+    formdata.append("target_url", inputUrl);
+    const requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    };
+
+    fetch("https://uus.animemoe.us/api/shorten/", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if ("slug" in result) {
+          // append the result to array `shortenedLink`
+          setShortenedLink((shortenedLink) => [
+            ...shortenedLink,
+            {
+              original: inputUrl,
+              result: `${process.env.domain}/${result.slug}`,
+            },
+          ]);
+        } else {
+          alert("Link yang dimasukkan tidak valid.");
+        }
+      })
+      .catch((error) => {
+        alert("Sedang ada masalah. Coba lagi nanti.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div className="min-vh-100 ">
       <Navbar />
+
+
       {/* start URL shortener */}
       <div
         className="container-fluid d-flex align-items-center justify-content-center"
@@ -29,21 +82,99 @@ export default function Home(props) {
                   type="text"
                   className="form-control"
                   placeholder="Masukan Link..."
+                  onChange={(e) => setInputUrl(e.target.value)}
+                  value={inputUrl}
+
                 />
               </div>
             </div>
             <div className="col-md-4 p-1 animate__animated animate__fadeInRight">
               <div className="input-group input-group-lg">
-                <button type="button" className="btn btn-primary w-100">
-                  Singkatkan!
-                </button>
+                {isLoading === true && (
+                  <button
+                    type="button"
+                    className="btn btn-primary w-100 disabled"
+                  >
+                    Sabar...
+                  </button>
+                )}
+                {isLoading === false && (
+                  <button
+                    type="button"
+                    className="btn btn-primary w-100"
+                    onClick={handleSubmitButton}
+                  >
+                    Singkatkan!
+                  </button>
+                )}
               </div>
             </div>
           </div>
         </div>
       </div>
       {/* end URL shortener */}
+
+      {/* result */}
       <div className="container-fluid border">
+        <div className="p-1 m-3 mb-3">
+          <div
+            className="row bg-white rounded shadow"
+            style={{ marginTop: "-10rem" }}
+          >
+            {/* shortener link result */}
+            {shortenedLink.length > 0 && (
+              <div className="rounded py-1 px-1">
+                {shortenedLink.map((e, i) => {
+                  return (
+                    <div
+                      className="list-group-item  animate__animated animate__fadeIn"
+                      key={i}
+                    >
+                      <div className="row">
+                        {/* original url */}
+                        <div className="col-sm-6 my-1">
+                          <a
+                            href={e.original}
+                            className="text-decoration-none text-muted"
+                          >
+                            {e.original}
+                          </a>
+                        </div>
+                        {/* end — original url */}
+                        {/* result url */}
+                        <div className="col-sm-5 my-1">
+                          <a
+                            href={e.result}
+                            className="text-decoration-none text-muted"
+                          >
+                            {e.result}
+                          </a>
+                        </div>
+                        {/* end — result url */}
+                        {/* copy button */}
+                        <div className="col-sm-1 my-1">
+                          <div className="d-grid">
+                            <CopyToClipboard text={e.result}>
+                              <button
+                                type="button"
+                                className="btn btn-outline-secondary btn-sm shadow-none"
+                              >
+                                Copy
+                              </button>
+                            </CopyToClipboard>
+                          </div>
+                        </div>
+                        {/* end — copy button */}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+            {/* end —  shortener link result */}
+          </div>
+        </div>
+        {/* end result */}
         <div className="row">
           <div className="col-md-4 p-3">
             <div className="border rounded shadow p-4">
