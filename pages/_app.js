6bf118cdb3698418fd82498e6979bebb75e33615
Arter Tendean <arter.tendean.07@gmail.com>
2022-04-21 19:15:41 +0800

    Choc UI (#58)
    * adding choc-ui
    
    * update home page
    
    * finished home page
    
    * adding next/head
    
    * update navbar link
    
    * update footer

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,10 @@ import "../styles/nprogress.css";
 
 import Router from "next/router";
 import NProgress from "nprogress";
+// import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, createStandaloneToast } from "@chakra-ui/react";
+
+const { toast, ToastContainer } = createStandaloneToast();
 
 // nprogress loading
 Router.events.on("routeChangeStart", () => NProgress.start());
@@ -12,7 +16,14 @@ Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,218 +1,206 @@
+import Footer from "../components/molecules/Footer";
 import Head from "next/head";
 import Navbar from "../components/molecules/Navbar";
+import React, { useState } from "react";
+import copy from "copy-to-clipboard";
 
-import { useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useToast } from "@chakra-ui/react";
+import {
+  chakra,
+  Box,
+  useColorModeValue,
+  Flex,
+  SimpleGrid,
+  GridItem,
+  VisuallyHidden,
+  Input,
+  Button,
+  Stack,
+  Icon,
+} from "@chakra-ui/react";
 
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
-import Spinner from "react-bootstrap/Spinner";
+const KuttyHero = () => {
+  const toast = useToast();
 
-export default function Home(props) {
+  const Feature = (props) => (
+    <Flex alignItems="center" color={useColorModeValue(null, "white")}>
+      <Icon
+        boxSize={4}
+        mr={1}
+        color="green.600"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+          clipRule="evenodd"
+        ></path>
+      </Icon>
+      {props.children}
+    </Flex>
+  );
+
+  const [isLoading, setIsloading] = useState(false);
   const [inputUrl, setInputUrl] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [shortenedLink, setShortenedLink] = useState([]);
 
-  const handleSubmitButton = (button) => {
-    setIsLoading(true);
+  const handleSubmitButton = () => {
+    setIsloading(true);
 
-    // inputUrl cannot be empty
     if (inputUrl.length <= 0) {
-      alert("Masukan link!");
-      setIsLoading(false);
+      toast({
+        description: "Tautan tidak boleh kosong!",
+        status: "warning",
+        duration: 1300,
+        isClosable: true,
+        position: "bottom-right",
+      });
+      setIsloading(false);
       return;
     }
 
     // API calling
-    const formdata = new FormData();
+    let formdata = new FormData();
     formdata.append("target_url", inputUrl);
-    const requestOptions = {
+    let requestOptions = {
       method: "POST",
       body: formdata,
       redirect: "follow",
     };
 
-    fetch("https://uus.animemoe.us/api/shorten/", requestOptions)
+    fetch(`${process.env.rootApiEndpoint}/api/shorten/`, requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        if ("slug" in result) {
-          // append the result to array `shortenedLink`
-          setShortenedLink((shortenedLink) => [
-            ...shortenedLink,
-            {
-              original: inputUrl,
-              result: `${process.env.domain}/${result.slug}`,
-            },
-          ]);
+        if (result.slug) {
+          // copy to clipboard
+          copy(`${process.env.domain}/${result.slug}`);
 
-          // clean the input url
-          setInputUrl("");
+          toast({
+            description: "Tautan telah disalin ke clipboard.",
+            status: "success",
+            duration: 2000,
+            isClosable: true,
+            position: "bottom-right",
+          });
         } else {
-          alert("Link yang dimasukkan tidak valid.");
+          toast({
+            description: "Tautan yang diberikan tidak valid.",
+            status: "warning",
+            duration: 1300,
+            isClosable: true,
+            position: "bottom-right",
+          });
         }
       })
       .catch((error) => {
-        alert("Sedang ada masalah. Coba lagi nanti.");
+        toast({
+          description: "Server sedang main tenis 🙃",
+          status: "info",
+          duration: 1300,
+          isClosable: true,
+          position: "bottom-right",
+        });
       })
       .finally(() => {
-        setIsLoading(false);
+        setIsloading(false);
+        setInputUrl("");
       });
   };
 
   return (
-    <div className="min-vh-100 ">
+    <>
       <Head>
         <title>Unklab URL Shortener 🔥</title>
       </Head>
-
       <Navbar />
-
-      {/* start URL shortener */}
-      <div
-        className="container-fluid d-flex align-items-center justify-content-center shadow"
-        style={{
-          minHeight: "33rem",
-          backgroundImage: `url(/img/bg/nayam-oeAcCi49Blg-unsplash.webp)`,
-          backrepeat: "no-repeat",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="container-md" style={{ marginTop: "0rem" }}>
-          <h1 className="text-center text-white mb-5">
+      <Box px={4} py={32} mx="auto">
+        <Box
+          w={{ base: "full", md: 11 / 12, xl: 8 / 12 }}
+          textAlign={{ base: "left", md: "center" }}
+          mx="auto"
+        >
+          <chakra.h1
+            mb={3}
+            fontSize={{ base: "4xl", md: "5xl" }}
+            fontWeight={{ base: "bold", md: "extrabold" }}
+            color={useColorModeValue("gray.900", "gray.100")}
+            lineHeight="shorter"
+          >
             Penyingkat Tautan Universitas Klabat
-          </h1>
-
-          <div className="row">
-            <InputGroup size="lg" className="mb-3 p-0 shadow rounded">
-              <FormControl
-                placeholder="Masukkan Link..."
-                className="p-2 shadow-none"
+          </chakra.h1>
+          <chakra.p
+            mb={6}
+            fontSize={{ base: "lg", md: "xl" }}
+            color="gray.500"
+            lineHeight="base"
+          >
+            “The difference between ordinary and extraordinary is that little
+            extra.”
+          </chakra.p>
+          <SimpleGrid
+            as="form"
+            w={{ base: "full", md: 7 / 12 }}
+            columns={{ base: 1, lg: 6 }}
+            spacing={3}
+            pt={1}
+            mx="auto"
+            mb={8}
+          >
+            <GridItem as="label" colSpan={{ base: "auto", lg: 4 }}>
+              <VisuallyHidden>Masukkan Tautan</VisuallyHidden>
+              <Input
+                mt={0}
+                size="lg"
+                type="text"
+                placeholder="Masukkan link..."
+                required
                 onChange={(e) => setInputUrl(e.target.value)}
                 value={inputUrl}
               />
-              <Button
-                variant="dark"
-                className="shadow-none"
-                disabled={isLoading}
-                onClick={handleSubmitButton}
-              >
-                {isLoading ? (
-                  <>
-                    <Spinner animation="border" size="sm" />
-                    {"  "}
-                    Sabar........
-                  </>
-                ) : (
-                  "Singkatkan!"
-                )}
-              </Button>
-            </InputGroup>
-          </div>
-        </div>
-      </div>
-      {/* end URL shortener */}
-
-      {/* result */}
-      <div className="container-fluid border">
-        <div className="p-1 m-3 mb-3">
-          <div
-            className="row bg-white rounded shadow"
-            style={{ marginTop: "-10rem" }}
+            </GridItem>
+            <Button
+              as={GridItem}
+              w="full"
+              variant="solid"
+              colSpan={{ base: "auto", lg: 2 }}
+              size="lg"
+              type="submit"
+              colorScheme="purple"
+              cursor="pointer"
+              isLoading={isLoading}
+              loadingText="Sabar..."
+              onClick={handleSubmitButton}
+              // onClick={() =>
+              //   toast({
+              //     title: "Account created.",
+              //     description: "We've created your account for you.",
+              //     status: "success",
+              //     duration: 9000,
+              //     isClosable: true,
+              //   })
+              // }
+            >
+              Singkatkan!
+            </Button>
+          </SimpleGrid>
+          <Stack
+            display="flex"
+            direction={{ base: "column", md: "row" }}
+            justifyContent={{ base: "start", md: "center" }}
+            mb={3}
+            spacing={{ base: 2, md: 8 }}
+            fontSize="xs"
+            color="gray.600"
           >
-            {/* shortener link result */}
-            {shortenedLink.length > 0 && (
-              <div className="rounded py-1 px-1">
-                {shortenedLink.map((e, i) => {
-                  return (
-                    <div
-                      className="list-group-item  animate__animated animate__fadeIn"
-                      key={i}
-                    >
-                      <div className="row">
-                        {/* original url */}
-                        <div className="col-sm-6 my-1">
-                          <a
-                            href={e.original}
-                            className="text-decoration-none text-muted"
-                          >
-                            {e.original}
-                          </a>
-                        </div>
-                        {/* end — original url */}
-                        {/* result url */}
-                        <div className="col-sm-5 my-1">
-                          <a
-                            href={e.result}
-                            className="text-decoration-none text-muted"
-                          >
-                            {e.result}
-                          </a>
-                        </div>
-                        {/* end — result url */}
-                        {/* copy button */}
-                        <div className="col-sm-1 my-1">
-                          <div className="d-grid">
-                            <CopyToClipboard text={e.result}>
-                              <button
-                                type="button"
-                                className="btn btn-outline-secondary btn-sm shadow-none"
-                              >
-                                Copy
-                              </button>
-                            </CopyToClipboard>
-                          </div>
-                        </div>
-                        {/* end — copy button */}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-            {/* end —  shortener link result */}
-          </div>
-        </div>
-        {/* end result */}
-        <div className="row">
-          <div className="col-md-4 p-3">
-            <div className="border rounded shadow p-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                rerum ipsam omnis. Assumenda molestias cupiditate nisi
-                recusandae, quibusdam ut nesciunt at quae. Eaque nulla provident
-                soluta harum laudantium, deleniti excepturi?
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4 p-3">
-            <div className="border rounded shadow p-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                rerum ipsam omnis. Assumenda molestias cupiditate nisi
-                recusandae, quibusdam ut nesciunt at quae. Eaque nulla provident
-                soluta harum laudantium, deleniti excepturi?
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4 p-3">
-            <div className="border rounded shadow p-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                rerum ipsam omnis. Assumenda molestias cupiditate nisi
-                recusandae, quibusdam ut nesciunt at quae. Eaque nulla provident
-                soluta harum laudantium, deleniti excepturi?
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            <Feature>Project Final Fullstack FIK Unklab</Feature>
+            <Feature>NextJs</Feature>
+            <Feature>Django</Feature>
+          </Stack>
+        </Box>
+      </Box>
+      <Footer />
+    </>
   );
-}
+};
 
-export async function getServerSideProps(context) {
-  const data = {};
-
-  return { props: { data } };
-}
+export default KuttyHero;
